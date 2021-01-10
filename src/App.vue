36a40674bc40f34f4a0737e5ddb6a4fb9f27c8f1
Niroshkumar072 <niroshkumar072@gmail.com>
2021-01-10 18:20:19 +0530

    finished project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 style='color:purple'>ReflexClick</h1>
+  <button @click="start" :disabled='isPlaying'>Shoot-Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end='endgame' />
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results},
+  data() {
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  }, 
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endgame(time){
+      this.score = time
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -20,7 +42,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: rgb(15, 15, 15);
   margin-top: 60px;
+}
+button{
+  background: red;
+  color:white;
+  border:none;
+  padding:8px 16px;
+  border-radius:4px;
+  font-size:16px;
+  letter-spacing:1px;
+  cursor:pointer;
+  margin:10px;
+}
+button[disabled]{
+  opacity:0.2;
+  cursor:not-allowed;
 }
 </style>
